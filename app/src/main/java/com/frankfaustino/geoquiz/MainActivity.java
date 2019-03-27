d595997150347119ebcf6f85bc78387b2b059c78
Frank Faustino <frankfaustino@gmail.com>
2019-03-27 11:49:39 -0700

    Add buttons and onClickListeners

--- app/src/main/java/com/frankfaustino/geoquiz/MainActivity.java
+++ app/src/main/java/com/frankfaustino/geoquiz/MainActivity.java
@@ -2,12 +2,32 @@ package com.frankfaustino.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mTrueButton;
+    private Button mFalseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        System.out.print(R.string.true_button);
+        mTrueButton = findViewById(R.id.true_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Does nothing yet
+            }
+        });
+        mFalseButton =  findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Does nothing yet
+            }
+        });
     }
 }
